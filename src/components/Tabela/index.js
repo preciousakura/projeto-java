61f9227af6eed66084f6f18ed5fd8d40e1ed6912
Matheusriale <matheusriale@alu.ufc.br>
2021-08-23 17:58:57 -0300

    ajuste no readme

--- src/components/Tabela/index.js
+++ src/components/Tabela/index.js
@@ -45,7 +45,7 @@ export function Tabela() {
       setLoading(false)
     };
     response();
-  }, [setDados, setselectEstado]);
+  }, [setDados, setselectEstado, confirmEditState]);
 
   
   const HandleValue = (e, indexMes, indexDado) => {
